0bdcddfdea667827f119795492bf08c8e69b6dd7
Frank de Jonge <info@frenky.net>
2018-03-19 22:37:41 +0100

    Updated a docblock hint, altered php version hinting

--- src/AggregateRootBehaviour/ConstructionBehaviour.php
+++ src/AggregateRootBehaviour/ConstructionBehaviour.php
@@ -42,7 +42,7 @@ trait ConstructionBehaviour
     {
         $aggregateRoot = new static($aggregateRootId);
 
-        /** @var Event $event */
+        /** @var object $event */
         foreach ($events as $event) {
             $aggregateRoot->apply($event);
             ++$aggregateRoot->aggregateRootVersion;
