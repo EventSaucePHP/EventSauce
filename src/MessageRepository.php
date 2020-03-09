10e59867dfdf55d705fdad07dbb317d1594ed3c3
Frank de Jonge <info@frenky.net>
2020-03-09 19:43:31 +0100

    Declare return-type for message repository with phpastan generic notation

--- src/MessageRepository.php
+++ src/MessageRepository.php
@@ -10,7 +10,13 @@ interface MessageRepository
 {
     public function persist(Message ...$messages);
 
+    /**
+     * @return Generator<Message>
+     */
     public function retrieveAll(AggregateRootId $id): Generator;
 
+    /**
+     * @return Generator<Message>
+     */
     public function retrieveAllAfterVersion(AggregateRootId $id, int $aggregateRootVersion): Generator;
 }
