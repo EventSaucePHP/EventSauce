9b80680a0f14ad68f198e1ea9fdcc058a72bb4e5
Frank de Jonge <info@frenky.net>
2020-04-04 18:22:48 +0200

    Use marker interface for command in testing example

---
+++ src/TestUtilities/TestingAggregates/PerformDummyTask.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EventSauce\EventSourcing\TestUtilities\TestingAggregates;
+
+use EventSauce\EventSourcing\AggregateRootId;
+
+class PerformDummyTask
+{
+    /**
+     * @var AggregateRootId
+     */
+    private $aggregateRootId;
+
+    public function __construct(AggregateRootId $aggregateRootId)
+    {
+        $this->aggregateRootId = $aggregateRootId;
+    }
+
+    public function aggregateRootId(): AggregateRootId
+    {
+        return $this->aggregateRootId;
+    }
+}
