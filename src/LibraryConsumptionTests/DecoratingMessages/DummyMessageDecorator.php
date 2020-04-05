6f63fa012b078c7f195ed9842208005790fa5c47
Frank de Jonge <info@frenky.net>
2020-04-05 14:37:13 +0200

    Renamed module

--- src/LibraryConsumptionTests/DecoratingMessages/DummyMessageDecorator.php
+++ src/LibraryConsumptionTests/DecoratingMessages/DummyMessageDecorator.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace EventSauce\EventSourcing\Integration\DecoratingMessages;
+namespace EventSauce\EventSourcing\LibraryConsumptionTests\DecoratingMessages;
 
 use EventSauce\EventSourcing\Message;
 use EventSauce\EventSourcing\MessageDecorator;
