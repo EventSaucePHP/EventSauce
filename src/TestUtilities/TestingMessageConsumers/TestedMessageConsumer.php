9dcb3b7b9121cc191f99f0144444d2b6d27a1650
Frank de Jonge <info@frenky.net>
2020-05-24 22:01:28 +0200

    Added  message consumer test case.

---
+++ src/TestUtilities/TestingMessageConsumers/TestedMessageConsumer.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EventSauce\EventSourcing\TestUtilities\TestingMessageConsumers;
+
+use EventSauce\EventSourcing\Message;
+use EventSauce\EventSourcing\MessageConsumer;
+use LogicException;
+
+class TestedMessageConsumer implements MessageConsumer
+{
+    /**
+     * @var int
+     */
+    private $failAfterNumberOfMessages;
+
+    /**
+     * @var int
+     */
+    private $numberOfMessagesProcessed = 0;
+
+    public function __construct(int $failAfterNumberOfMessages)
+    {
+        $this->failAfterNumberOfMessages = $failAfterNumberOfMessages;
+    }
+
+    public function handle(Message $message)
+    {
+        if (++$this->numberOfMessagesProcessed === $this->failAfterNumberOfMessages) {
+            throw new LogicException("Too many messages");
+        }
+    }
+
+    public function numberOfMessagesProcessed(): int
+    {
+        return $this->numberOfMessagesProcessed;
+    }
+}
---
+++ src/TestUtilities/TestingMessageConsumers/TestedMessageConsumerTest.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EventSauce\EventSourcing\TestUtilities\TestingMessageConsumers;
+
+use DomainException;
+use EventSauce\EventSourcing\Message;
+use EventSauce\EventSourcing\MessageConsumer;
+use EventSauce\EventSourcing\TestUtilities\MessageConsumerTestCase;
+use LogicException;
+
+class TestedMessageConsumerTest extends MessageConsumerTestCase
+{
+    public function messageConsumer(): MessageConsumer
+    {
+        return new TestedMessageConsumer(5);
+    }
+
+    /**
+     * @test
+     */
+    public function not_expecting_an_exception()
+    {
+        $this->expectException(LogicException::class);
+        $this->when(
+            new ConsumerEvent(),
+            new ConsumerEvent(),
+            new ConsumerEvent(),
+            new ConsumerEvent(),
+            new ConsumerEvent()
+        );
+        $this->assertScenario();
+    }
+
+    /**
+     * @test
+     */
+    public function expecting_an_exception(): void
+    {
+        $this->when(
+            new ConsumerEvent(),
+            new ConsumerEvent(),
+            new ConsumerEvent(),
+            new ConsumerEvent(),
+            new ConsumerEvent()
+        );
+        $this->expectToFail(new LogicException("Too many messages"));
+        $this->assertScenario();
+    }
+
+    /**
+     * @test
+     */
+    public function expecting_an_incorrect_exception(): void
+    {
+        $this->expectException(LogicException::class);
+        $this->given(
+            new ConsumerEvent(),
+            new ConsumerEvent(),
+            new ConsumerEvent()
+        )->when(
+            new ConsumerEvent(),
+            new ConsumerEvent()
+        );
+        $this->expectToFail(new DomainException("Wrong exception"));
+        $this->assertScenario();
+    }
+
+    /**
+     * @test
+     */
+    public function expecting_assertions(): void
+    {
+        $this->when(new Message(new ConsumerEvent()), new ConsumerEvent())
+            ->then(function(TestedMessageConsumer $consumer) {
+                $this->assertEquals(2, $consumer->numberOfMessagesProcessed());
+            });
+    }
+}
