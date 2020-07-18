1b4e4ce27e1dff53621cdb695a3fccaf85674139
Frank de Jonge <info@frenky.net>
2020-07-18 20:01:17 +0200

    Add decorating message dispatcher

---
+++ src/DecoratingMessageDispatcher.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EventSauce\EventSourcing;
+
+class DecoratingMessageDispatcher implements MessageDispatcher
+{
+    /**
+     * @var MessageDispatcher
+     */
+    private $dispatcher;
+
+    /**
+     * @var MessageDecorator
+     */
+    private $decorator;
+
+    public function __construct(MessageDispatcher $dispatcher, MessageDecorator $decorator = null)
+    {
+        $this->dispatcher = $dispatcher;
+        $this->decorator = $decorator ?: new DefaultHeadersDecorator();
+    }
+
+    public function dispatch(Message ...$messages)
+    {
+        $messages = array_map(
+            function ($message) {
+                return $this->decorator->decorate($message);
+            },
+            $messages
+        );
+
+        $this->dispatcher->dispatch(...$messages);
+    }
+}
