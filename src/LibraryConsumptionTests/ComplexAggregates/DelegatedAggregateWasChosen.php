b1af59e012d00fe824b492277d16af7a1a0e5a5d
Frank de Jonge <info@frenky.net>
2020-10-18 22:24:15 +0200

    Implemented behavior to implement complex aggregate roots with inner aggregates.

---
+++ src/LibraryConsumptionTests/ComplexAggregates/DelegatedAggregateWasChosen.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EventSauce\EventSourcing\LibraryConsumptionTests\ComplexAggregates;
+
+use EventSauce\EventSourcing\Serialization\SerializablePayload;
+
+class DelegatedAggregateWasChosen implements SerializablePayload
+{
+    public function toPayload(): array
+    {
+        return [];
+    }
+
+    public static function fromPayload(array $payload): SerializablePayload
+    {
+        return new DelegatedAggregateWasChosen();
+    }
+}
