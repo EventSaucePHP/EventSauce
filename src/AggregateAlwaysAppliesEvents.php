6a261fe76681b004213fce79a76e31023ef9735e
Frank de Jonge <info@frankdejonge.nl>
2021-03-31 20:58:17 +0200

    Added Trait description docblocks

--- src/AggregateAlwaysAppliesEvents.php
+++ src/AggregateAlwaysAppliesEvents.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace EventSauce\EventSourcing;
 
+/**
+ * This trait calls the apply[EventClassName] method
+ * for every event it receives. Use this trait if
+ * you want to ensure that you do not miss responding
+ * to any type of event you raise in the aggregate.
+ */
 trait AggregateAlwaysAppliesEvents
 {
     private int $aggregateRootVersion = 0;
