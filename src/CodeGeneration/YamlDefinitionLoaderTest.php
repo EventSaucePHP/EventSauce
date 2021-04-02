81523d4f5d1ec0b5316d005e08ef8c909c786fcd
Frank de Jonge <info@frankdejonge.nl>
2021-04-02 09:24:09 +0200

    Renable test cases

--- src/CodeGeneration/YamlDefinitionLoaderTest.php
+++ src/CodeGeneration/YamlDefinitionLoaderTest.php
@@ -42,9 +42,9 @@ class YamlDefinitionLoaderTest extends TestCase
     public function definitionProvider()
     {
         yield [__DIR__ . '/Fixtures/exampleDefinition.yaml', __DIR__ . '/Fixtures/definedWithYamlFixture.php'];
-//        yield [__DIR__ . '/Fixtures/exampleDefinitionWithoutHelpers.yaml', __DIR__ . '/Fixtures/definedWithoutHelpersInYamlFixture.php', false];
-//        yield [__DIR__ . '/Fixtures/definitionWithFieldsFromOtherDefinitions.yaml', __DIR__ . '/Fixtures/definitionWithFieldsFromOtherDefinitionsFixture.php', false];
-//        yield [__DIR__ . '/Fixtures/commands-with-interfaces.yaml', __DIR__ . '/Fixtures/commandsWithInterfaces.php', false];
+        yield [__DIR__ . '/Fixtures/exampleDefinitionWithoutHelpers.yaml', __DIR__ . '/Fixtures/definedWithoutHelpersInYamlFixture.php', false];
+        yield [__DIR__ . '/Fixtures/definitionWithFieldsFromOtherDefinitions.yaml', __DIR__ . '/Fixtures/definitionWithFieldsFromOtherDefinitionsFixture.php', false];
+        yield [__DIR__ . '/Fixtures/commands-with-interfaces.yaml', __DIR__ . '/Fixtures/commandsWithInterfaces.php', false];
     }
 
     /**
