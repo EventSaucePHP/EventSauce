c22d75a2fb36e4736c9d757f1efcd457b657c598
Frank de Jonge <info@frankdejonge.nl>
2021-08-20 17:06:54 +0200

    Added decorator for serialization to store dates in the new format.

---
+++ src/Serialization/MySQL8DateFormatting.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EventSauce\EventSourcing\Serialization;
+
+use EventSauce\EventSourcing\Header;
+use EventSauce\EventSourcing\Message;
+use function substr;
+
+class MySQL8DateFormatting implements MessageSerializer
+{
+    public function __construct(
+        private MessageSerializer $messageSerializer,
+        private string $field = Header::TIME_OF_RECORDING,
+    ) {
+    }
+
+    public function serializeMessage(Message $message): array
+    {
+        $payload = $this->messageSerializer->serializeMessage($message);
+        $dateTimeString = $payload['headers'][$this->field] ?? null;
+
+        if (null !== $dateTimeString) {
+            $payload['headers'][$this->field] = substr($dateTimeString, 0, -2) . ':' . substr($dateTimeString, -2);
+        }
+
+        return $payload;
+    }
+
+    public function unserializePayload(array $payload): Message
+    {
+        $dateTimeString = $payload['headers'][$this->field] ?? null;
+
+        if (null !== $dateTimeString) {
+            $payload['headers'][$this->field] = substr($dateTimeString, 0, -3) . substr($dateTimeString, -2);
+        }
+
+        return $this->messageSerializer->unserializePayload($payload);
+    }
+}
---
+++ src/Serialization/MySQL8DateFormattingTest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EventSauce\EventSourcing\Serialization;
+
+use PHPUnit\Framework\TestCase;
+
+class MySQL8DateFormattingTest extends TestCase
+{
+}
