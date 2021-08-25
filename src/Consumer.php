a963e98a9991a988e0048ec498f3dbc0ffd05787
Frank de Jonge <info@frankdejonge.nl>
2021-08-25 23:13:35 +0200

    Make upgrading easier by supplying a deprecated interface.

---
+++ src/Consumer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace EventSauce\EventSourcing;
+
+/**
+ * @deprecated use the MessageConsumer interface instead.
+ * @see MessageConsumer
+ */
+interface Consumer extends MessageConsumer
+{
+}
