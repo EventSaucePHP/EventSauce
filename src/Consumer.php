855c08b185c1e2769a938d5850bd0af90de501ae
Frank de Jonge <info@frankdejonge.nl>
2021-08-25 23:14:52 +0200

    CS fix

--- src/Consumer.php
+++ src/Consumer.php
@@ -1,9 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace EventSauce\EventSourcing;
 
 /**
- * @deprecated use the MessageConsumer interface instead.
+ * @deprecated use the MessageConsumer interface instead
  * @see MessageConsumer
  */
 interface Consumer extends MessageConsumer
