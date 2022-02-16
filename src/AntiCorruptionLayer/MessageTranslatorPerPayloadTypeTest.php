fbd5bbae592e7bf73be9ed8cffbe5f390d8302b5
Frank de Jonge <info@frankdejonge.nl>
2022-02-16 10:13:36 +0100

    Added translator that has a type-to-translator mapping.

---
+++ src/AntiCorruptionLayer/MessageTranslatorPerPayloadTypeTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EventSauce\EventSourcing\AntiCorruptionLayer;
+
+use EventSauce\EventSourcing\Message;
+use PHPUnit\Framework\TestCase;
+
+class MessageTranslatorPerPayloadTypeTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider dpTransformationExamples
+     */
+    public function messages_are_routed_to_the_right_translator(Message $input, Message $expected): void
+    {
+        $translators = [
+            StubPublicEvent::class => new class implements MessageTranslator {
+                public function translateMessage(Message $message): Message
+                {
+                    return $message->withHeader('x', 'public');
+                }
+            },
+            StubPrivateEvent::class => new class implements MessageTranslator {
+                public function translateMessage(Message $message): Message
+                {
+                    return $message->withHeader('x', 'private');
+                }
+            },
+        ];
+
+        $translator = new MessageTranslatorPerPayloadType($translators);
+
+        $actual = $translator->translateMessage($input);
+
+        self::assertEquals($expected, $actual);
+    }
+
+    public function dpTransformationExamples(): iterable
+    {
+        yield [new Message(new StubPublicEvent('yes')), new Message(new StubPublicEvent('yes'), ['x' => 'public'])];
+        yield [new Message(new StubPrivateEvent('yes')), new Message(new StubPrivateEvent('yes'), ['x' => 'private'])];
+        yield [new Message(new StubExcludedEvent('yes')), new Message(new StubExcludedEvent('yes'))];
+    }
+}
