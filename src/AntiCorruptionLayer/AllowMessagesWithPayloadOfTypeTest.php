16e351aa29002cdd0fe1fa604f813a9e8099345f
Robertbaelde <robert_baelde@hotmail.com>
2022-02-17 16:07:40 +0100

    Add test

---
+++ src/AntiCorruptionLayer/AllowMessagesWithPayloadOfTypeTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace EventSauce\EventSourcing\AntiCorruptionLayer;
+
+use EventSauce\EventSourcing\Message;
+use PHPUnit\Framework\TestCase;
+
+class AllowMessagesWithPayloadOfTypeTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider dpMessagesWithPayloadOfType
+     */
+    public function matching_all_filters(object $event, bool $expectedOutcome): void
+    {
+        $filter = new AllowMessagesWithPayloadOfType(StubPublicEvent::class, StubPrivateEvent::class);
+
+        $result = $filter->allows(new Message($event));
+
+        $this->assertEquals($expectedOutcome, $result);
+    }
+
+    public function dpMessagesWithPayloadOfType(): iterable
+    {
+        yield [new StubPublicEvent('yes'), true];
+        yield [new StubPrivateEvent('yes'), true];
+        yield [new StubExcludedEvent('yes'), false];
+    }
+}
