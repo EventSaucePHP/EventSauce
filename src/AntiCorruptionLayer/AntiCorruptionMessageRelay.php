c0665c347b43efc45035f5b935b17dda938b472c
Frank de Jonge <info@frankdejonge.nl>
2022-02-17 19:18:10 +0100

    Added ACL MessageRelay (Consumer -> Dispatcher)

---
+++ src/AntiCorruptionLayer/AntiCorruptionMessageRelay.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EventSauce\EventSourcing\AntiCorruptionLayer;
+
+use EventSauce\EventSourcing\Message;
+use EventSauce\EventSourcing\MessageConsumer;
+use EventSauce\EventSourcing\MessageDispatcher;
+
+class AntiCorruptionMessageRelay implements MessageConsumer
+{
+    private MessageFilter $filterBefore;
+    private MessageFilter $filterAfter;
+
+    public function __construct(
+        private MessageDispatcher $dispatcher,
+        private MessageTranslator $translator,
+        MessageFilter $filterBefore = null,
+        MessageFilter $filterAfter = null,
+    )
+    {
+        $this->filterBefore = $filterBefore ?? new AllowAllMessages();
+        $this->filterAfter = $filterAfter ?? new AllowAllMessages();
+    }
+
+    public function handle(Message $message): void
+    {
+        if ( ! $this->filterBefore->allows($message)) {
+            return;
+        }
+
+        $message = $this->translator->translateMessage($message);
+
+        if ( ! $this->filterAfter->allows($message)) {
+            return;
+        }
+
+        $this->dispatcher->dispatch($message);
+    }
+}
