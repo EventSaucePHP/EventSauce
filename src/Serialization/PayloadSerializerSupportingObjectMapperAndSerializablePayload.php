0bc51c4fc70997eb390a825401113a0860f0d635
Frank de Jonge <info@frankdejonge.nl>
2022-06-12 20:48:19 +0200

    Added implementation that respects both forms of serialization.

---
+++ src/Serialization/PayloadSerializerSupportingObjectMapperAndSerializablePayload.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EventSauce\EventSourcing\Serialization;
+
+use function is_a;
+
+class PayloadSerializerSupportingObjectMapperAndSerializablePayload implements PayloadSerializer
+{
+    private ConstructingPayloadSerializer $constructingSerializer;
+    private ObjectMapperPayloadSerializer $objectMapperSerializer;
+
+    public function __construct(
+        ConstructingPayloadSerializer $constructingSerializer = null,
+        ObjectMapperPayloadSerializer $objectMapperSerializer = null,
+    )
+    {
+        $this->constructingSerializer = $constructingSerializer ?? new ConstructingPayloadSerializer();
+        $this->objectMapperSerializer = $objectMapperSerializer ?? new ObjectMapperPayloadSerializer();
+    }
+
+    public function serializePayload(object $event): array
+    {
+        if ($event instanceof SerializablePayload) {
+            return $this->constructingSerializer->serializePayload($event);
+        }
+
+        return $this->objectMapperSerializer->serializePayload($event);
+    }
+
+    public function unserializePayload(string $className, array $payload): object
+    {
+        if (is_a($className, SerializablePayload::class, true)) {
+            return $this->constructingSerializer->unserializePayload($className, $payload);
+        }
+
+        return $this->objectMapperSerializer->unserializePayload($className, $payload);
+    }
+}
